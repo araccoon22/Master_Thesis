f432cdd95eb362d7ac6f20995decc642d0fd59f8
Alexia Paratte <alexia.paratte@epfl.ch>
2025-03-28 16:05:33 +0100

    Remove virtual environment from Git

--- pdm_env/Lib/site-packages/morphio/mut/__init__.py
+++
@@ -1,9 +0,0 @@
-from .._morphio.mut import (Morphology,
-                            Section,
-                            Soma,
-                            MitoSection,
-                            Mitochondria,
-                            GlialCell,
-                            EndoplasmicReticulum,
-                            DendriticSpine,
-                            )
--- pdm_env/Lib/site-packages/morphio/vasculature/__init__.py
+++
@@ -1,1 +0,0 @@
-from .._morphio.vasculature import Vasculature, Section
